4437b679e31eaa942204f04ee401a26d25337c8b
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-03 18:49:26 +0000

    removed comments from spec_helper

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,13 +7,8 @@ class Link
   property :id, Serial
   property :title, String
   property :url, String
-
 end
 
-# DataMapper::Logger.new($stdout, :debug)
-
 DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
-
 DataMapper.finalize
-
 DataMapper.auto_upgrade!
