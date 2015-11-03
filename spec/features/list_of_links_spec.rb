46eddbeaeefb2102949e598ccf0549227be77139
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-03 17:25:07 +0000

    added Gemfile and created first feature test: list of links on homepage

---
+++ spec/features/list_of_links_spec.rb
@@ -0,0 +1,10 @@
+feature 'list of links' do
+  scenario 'list of links appears on homepage' do
+    Link.create(title: 'Google', url: 'https://www.google.co.uk')
+    visit('/links')
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
+  end
+end
